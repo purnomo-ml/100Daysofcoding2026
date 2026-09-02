2c019c37680038befb4f8ba445a9282a1163dbca
Muh ma'ruf. <muhmakruf16@gmail.com>
2026-09-02 21:38:35 +0800

    Create Day01.java

---
+++ Day01.java
@@ -0,0 +1,13 @@
+/**
+ * Program sederhana untuk menampilkan nama.
+ */
+public class maruf {
+
+    /**
+     * Method utama untuk menjalankan program.
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Nama saya Ma'ruf");
+    }
+}
