175005ab7a999480c4bbfcb894f1c2c6913b0796
Muh ma'ruf. <muhmakruf16@gmail.com>
2026-09-04 13:20:27 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,18 @@
+public class day3 {
+    public static void main(String[] args) {
+
+        System.out.println("=== BIODATA ===\n");
+
+        System.out.println("Nama\t: Ma'ruf");
+        System.out.println("Umur\t: 19");
+        System.out.println("Prodi\t: Informatika");
+
+        System.out.println("\n=== APA KABAR DAY 3 ===");
+        System.out.println("\"masih aman\"");
+
+        System.out.println("\n=== FOLDER ===");
+        System.out.println("C:\\Users\\Java");
+
+        System.out.println("\n===selesai mi===");
+    }
+}
