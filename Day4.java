02c0bdc87380d68716726380282d474d01ea4613
Muh ma'ruf. <muhmakruf16@gmail.com>
2026-09-05 15:55:21 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte umur = 19;
+        short gulaaren = 1500;
+        int hargaLaptop = 7000000;
+        long mobil = 8000000000L;
+
+        System.out.println("Umur\t\t\t: " + umur);
+        System.out.println("Gula aren\t\t: " + gulaaren);
+        System.out.println("Harga Laptop\t\t: " + hargaLaptop);
+        System.out.println("mobil\t\t\t: " + mobil );
+    }
+}
