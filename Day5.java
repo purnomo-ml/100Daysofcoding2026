408697ea2c43efcca5a8194fb943b9f186484ca8
Muh ma'ruf. <muhmakruf16@gmail.com>
2026-09-06 01:36:59 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,15 @@
+public class day5 {
+    public static void main(String[] args) {
+
+        double tinggi = 170.5;
+        double berat = 60.75;
+
+        float suhu = 32.5f;
+        float jarak = 15.5f;
+
+        System.out.println("Tinggi : " + tinggi + " cm");
+        System.out.println("Berat  : " + berat + " kg");
+        System.out.println("Suhu   : " + suhu + " °C");
+        System.out.println("Jarak  : " + jarak + " km");
+    }
+}
