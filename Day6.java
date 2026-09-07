255bcceb8979fb404c9ef0f5a611ae378bd402a7
Muh ma'ruf. <muhmakruf16@gmail.com>
2026-09-07 18:43:37 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class day6 {
+    public static void main(String[] args) {
+
+        char nilai = 'A';
+        boolean lulus = true;
+
+        System.out.println("Nilai  : " + nilai);
+        System.out.println("Lulus  : " + lulus);
+    }
+}
