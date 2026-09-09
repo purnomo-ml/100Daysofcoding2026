7ad593336884e9927bda68cd5e98cd13c1626c8a
Muh ma'ruf. <muhmakruf16@gmail.com>
2026-09-09 18:46:06 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int umur = 19;
+        String nama = "Ma'ruf";
+        double tinggi = 168.7;
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+        System.out.println("tinggi : " + tinggi);
+       }
+}
