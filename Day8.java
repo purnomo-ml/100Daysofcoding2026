15dbd0a1714b0713996141c866a32a21a293a4f9
Muh ma'ruf. <muhmakruf16@gmail.com>
2026-09-10 00:09:53 +0800

    Update Day8.java

--- Day8.java
+++ Day8.java
@@ -1,12 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
-
-        int umur = 19;
-        String nama = "Ma'ruf";
-        double tinggi = 168.7;
-        
-        System.out.println("Nama : " + nama);
-        System.out.println("Umur : " + umur);
-        System.out.println("tinggi : " + tinggi);
-       }
+	public static void main(String[] args) {
+		 
+		 int umur = 99;
+		 
+		 System.out.println ( 1+umur );
+	}
 }
