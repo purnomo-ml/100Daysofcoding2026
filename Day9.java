6feb4909d01d9031ce24c2d58a5af82f3f06dc63
Muh ma'ruf. <muhmakruf16@gmail.com>
2026-09-10 09:07:02 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,10 @@
+public class day9 {
+    public static void main(String[] args) {
+
+        final double PI = 3.14;
+        final int UMUR_MINIMAL = 17;
+
+        System.out.println("Nilai PI          : " + PI);
+        System.out.println("Umur minimal      : " + UMUR_MINIMAL);
+    }
+}
