03ccd557f7ee33efaf7700fcc103ce0d132129ed
Muh ma'ruf. <muhmakruf16@gmail.com>
2026-09-11 18:27:57 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,13 @@
+public class day10  {
+    public static void main(String[] args) {
+
+        System.out.println("byte\t: " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+        System.out.println("short\t: " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
+        System.out.println("int\t: " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+        System.out.println("long\t: " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+        System.out.println("float\t: " + -Float.MAX_VALUE + " sampai " + Float.MAX_VALUE);
+        System.out.println("double\t: " + -Double.MAX_VALUE + " sampai " + Double.MAX_VALUE);
+        System.out.println("char\t: " + (int) Character.MIN_VALUE + " sampai " + (int) Character.MAX_VALUE);
+        System.out.println("boolean\t: true atau false");
+    }
+}
