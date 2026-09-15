8b263e565b89c2307275b480cffede1fa23c5cfd
Muh ma'ruf. <muhmakruf16@gmail.com>
2026-09-15 20:32:53 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int objek1 = 10;
+        int objek2 = 2;
+
+         int hasil1= objek1 + objek2; 
+         int hasil2 = objek1 - objek2; 
+        
+        System.out.println("hasil : " + hasil1);
+        System.out.println("hasil : " + hasil2);
+    }
+}
